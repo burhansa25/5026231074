185a767434897d29982258c15b3530f7d979c8d2
burhansa25 <bsacollegeone@gmail.com>
2025-06-15 21:57:36 +0700

    SIAP UAS

--- resources/views/x2/tambah.blade.php
+++ resources/views/x2/tambah.blade.php
@@ -4,7 +4,7 @@
 
 	<h3>Data Karyawan</h3>
 
-	<a href="/karyawan" class="btn btn-info">< Kembali</a>
+	<a href="/karyawan" class="btn btn-info">Kembali</a>
 
 	<br/>
 	<br/>
@@ -12,62 +12,54 @@
 	<form action="/karyawan/storeKaryawan" method="post" class="form-horizontal">
 		{{ csrf_field() }}
 
-        <div class="form-group has-success">
-            <label class="control-label col-sm-2" for="kodepegawai">
-                Kode Karyawan
-            </label>
-            <div class="col-sm-6" style="margin-bottom: 18px;">
+        <div class="form-group row">
+            <label class="col-sm-2 col-form-label" for="kodepegawai">Kode Karyawan</label>
+            <div class="col-sm-6">
                 <input class="form-control"
-                    type="text"
-                    id="kodepegawai"
-                    placeholder="Masukkan Kode Pegawai (Contoh: K0001)"
-                    name="kodepegawai"
-                    maxlength="5"
-                    required="required">
+                       type="text"
+                       id="kodepegawai"
+                       name="kodepegawai"
+                       placeholder="Masukkan Kode Pegawai (Contoh: K0001)"
+                       maxlength="5"
+                       required>
             </div>
         </div>
 
-        <div class="form-group has-success">
-            <label class="control-label col-sm-2" for="namalengkap">
-                Nama Lengkap
-            </label>
-            <div class="col-sm-6" style="margin-bottom: 18px;">
+        <div class="form-group row">
+            <label class="col-sm-2 col-form-label" for="namalengkap">Nama Lengkap</label>
+            <div class="col-sm-6">
                 <input class="form-control"
-                    type="text"
-                    id="namalengkap"
-                    placeholder="Masukkan Nama Lengkap Karyawan"
-                    name="namalengkap"
-                    maxlength="50"
-                    required="required">
+                       type="text"
+                       id="namalengkap"
+                       name="namalengkap"
+                       placeholder="Masukkan Nama Lengkap Karyawan"
+                       maxlength="50"
+                       required>
             </div>
         </div>
 
-        <div class="form-group has-success">
-            <label class="control-label col-sm-2" for="divisi">
-                Divisi
-            </label>
-            <div class="col-sm-6" style="margin-bottom: 18px;">
+        <div class="form-group row">
+            <label class="col-sm-2 col-form-label" for="divisi">Divisi</label>
+            <div class="col-sm-6">
                 <input class="form-control"
-                    type="text"
-                    id="divisi"
-                    placeholder="Masukkan Divisi (Contoh: HRD, IT)"
-                    name="divisi"
-                    maxlength="5"
-                    required="required">
+                       type="text"
+                       id="divisi"
+                       name="divisi"
+                       placeholder="Masukkan Divisi (Contoh: HRD, IT)"
+                       maxlength="5"
+                       required>
             </div>
         </div>
 
-        <div class="form-group has-success">
-            <label class="control-label col-sm-2" for="departemen">
-                Departemen
-            </label>
-            <div class="col-sm-6" style="margin-bottom: 18px;">
+        <div class="form-group row">
+            <label class="col-sm-2 col-form-label" for="departemen">Departemen</label>
+            <div class="col-sm-6">
                 <input class="form-control"
-                    type="number"
-                    id="departemen"
-                    placeholder="Masukkan Kode Departemen"
-                    name="departemen"
-                    required="required">
+                       type="number"
+                       id="departemen"
+                       name="departemen"
+                       placeholder="Masukkan Kode Departemen"
+                       required>
             </div>
         </div>
 
